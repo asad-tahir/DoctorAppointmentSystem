f002970a8e7890928aded01ca240d969d0a221ed
Asad Tahir <asadtahir.kund@gmail.com>
2020-12-08 17:08:20 +0500

    12/8/2020 5pm

--- Controllers/DoctorController.cs
+++ Controllers/DoctorController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DoctorAppointmentSystem.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,10 +7,20 @@ using System.Web.Mvc;
 
 namespace DoctorAppointmentSystem.Controllers
 {
+    [Authorize(Roles = UserType.Doctor)]
     public class DoctorController : Controller
     {
         // GET: Doctor
         public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CreateSlot()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateSlot(Slot slot)
         {
             return View();
         }
